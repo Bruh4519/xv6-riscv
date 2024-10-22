272f963dc8a232eed3807f682f2270dd578b47dd
Bruh4519 <benjaminpro4519@gmail.com>
2024-10-22 11:23:51 -0300

    Update test_priority.c

--- user/test_priority.c
+++ user/test_priority.c
@@ -1,1 +1,26 @@
+// En user/test_priority.c
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+void process_work(int i) {
+  printf("Ejecutando proceso %d\n", i);
+  sleep(5);  // Simula un trabajo de proceso
+  exit(0);
+}
+
+int main(void) {
+  int i;
+  for(i = 0; i < 20; i++) {
+    if(fork() == 0) {
+      process_work(i);
+    }
+  }
+
+  for(i = 0; i < 20; i++) {
+    wait(0);  // Espera a que todos los procesos hijos terminen
+  }
+
+  exit(0);
+}
 
